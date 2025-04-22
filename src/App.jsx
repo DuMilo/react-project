5c52a5bd968c48dc71b170ed4183c6813cce7be4
Milo Moreira <milomoreirac@gmail.com>
2025-04-21 21:23:15 -0300

    progressing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import { Header } from './components/Header';
-import { Post } from './Post';
-import './styles.css';
+import './global.css';
 
 export function App() {
   
   return (
     <div>
       <Header />
-      <Post 
-        author='Milo Moreira' 
-        content='Lorem ipsum'
-      />  
-      <Post 
-      author='River Pinotti' 
-      content='Você tá endauldi agrummit'
-      />  
     </div>
   )
 }
-
-
-
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import styles from './Header.module.css'
-
+import LBLogo from '../assets/leon-books-symbol.svg'
 
 export function Header() {
     return (
-        <header className={styles.header}>
-            <strong>Leon Books</strong>
-        </header>
+        <div className={styles.div}>
+            <header className={styles.header}>
+            <img src={LBLogo} alt='Logotipo do Leon Books' />
+            <p>
+                <span>Leon Books</span>
+            </p>
+            </header>
+        </div>
     )
 }
