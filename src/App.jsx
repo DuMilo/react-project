a2922adda6c970023cb03eb3ef36e72869f94878
Milo Moreira <milomoreirac@gmail.com>
2025-04-27 23:37:11 -0300

    sidebar and main made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Header } from './components/Header';
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
   
   return (
     <div>
       <Header />
+
+        <div className={styles.wrapper}>
+          <aside>
+            sidebar<br></br>
+            tres
+          </aside>
+          <main>aqui é o main</main>
+        </div>
     </div>
   )
 }
